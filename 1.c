a5071d4aaf49c36122f58ef287ff8fb4b00ed847
xx25yy <135425934+xx25yy@users.noreply.github.com>
2024-03-18 15:49:16 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct Week {
-    char day[100];
+    char *day;
     int date;
-    char actdisp[100];
+    char *actdisp;
 };
 
 struct Week day[7];
 
 void read() {
-    for (int i = 0; i < 7; i++) { // Correct loop condition
-        printf("Enter the date for week:\n");
-        scanf("%d", &day[i].date);
+    for (int i = 0; i < 7; i++) {
+        day[i].day = (char *)malloc(100 * sizeof(char)); // Allocate memory for the day
+        
 
         printf("Enter the Day for week:\n");
         scanf("%s", day[i].day);
 
+        printf("Enter the date for week:\n");
+        scanf("%d", &day[i].date);
+
+        day[i].actdisp = (char *)malloc(100 * sizeof(char)); // Allocate memory for the activity description
+        
+
         printf("Enter the activity description:\n");
-        scanf(" %[^\n]", day[i].actdisp); // Use %[^\n] to read the entire line
+        scanf(" %[^\n]", day[i].actdisp); // Read the entire line
     }
 }
 
 void display() {
     printf("------------Calendar---------\n");
 
-    for (int i = 0; i < 7; i++) { // Correct loop condition
+    for (int i = 0; i < 7; i++) {
         printf("Date:%d, \t Day:%s, \t Activity Description:%s \n", day[i].date, day[i].day, day[i].actdisp);
     }
 }
@@ -32,5 +40,7 @@ void display() {
 int main() {
     read();
     display();
+    
+    
     return 0;
 }
